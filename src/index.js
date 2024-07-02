b11ada00a898a6a462e440ec4f977847754a0192
Jacob Taylor <jtlikesps4@icloud.com>
2024-07-02 17:14:12 -0500

    Implemented calculateTotal method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 export function calculateTotal (items, tax) {
   // TODO
+  tax = Math.abs(tax);
+
+    // Initialize total cost to 0
+    let totalCost = 0;
+
+    // Iterate over each item in the items array
+    for (let item of items) {
+        // If the item is taxable, add the price plus the tax to the total cost
+        if (item.taxable) {
+            totalCost += item.price * (1 + tax);
+        } 
+        // If the item is not taxable, just add the price to the total cost
+        else {
+            totalCost += item.price;
+        }
+    }
+
+    // Return the total cost
+    return totalCost;
 }
